a91d17e616c572cab71f71b892be30cd1adce68a
Adam Fratino <adafrati@usnyclbw4784.global.publicisgroupe.net>
2018-01-12 18:55:52 -0500

    implement carousel and test with article grid

---
+++ _src/assets/javascripts/components/Carousel.js
@@ -0,0 +1,180 @@
+class Carousel {
+  constructor(carousel) {
+    this.carousel = carousel
+    this.container = this.carousel.querySelector('.carousel__slides')
+    this.previousButton = this.carousel.querySelector('.carousel__controls-prev')
+    this.nextButton = this.carousel.querySelector('.carousel__controls-next')
+    this.slides = Array.from(this.carousel.querySelectorAll('.carousel__slides > li'))
+    this.slideCount = this.slides.length
+
+    this.currentCount = 1
+    this.currentItem
+    this.activeItem
+    this.previousItem
+    this.nextItem
+
+    this.addEventListeners()
+  }
+
+  addEventListeners() {
+    // previous arrow moves to previous slide
+    this.previousButton.addEventListener('click', e => {
+      e.preventDefault()
+      this.currentCount === 1 ? this.currentCount = this.slideCount : this.currentCount--
+      this.transitionItem()
+    })
+
+    // next arrow moves to next slide
+    this.nextButton.addEventListener('click', e => {
+      e.preventDefault()
+      this.currentCount === this.slideCount ? this.currentCount = 1 : this.currentCount++
+      this.transitionItem()
+    })
+
+    document.addEventListener('DOMContentLoaded', () => {
+      // add data attributes to slides
+      this.slides.forEach((el, i) => el.setAttribute('data-slide', i+1))
+
+      // add data attributes to indicators
+      // this.dots.forEach((el, i) => el.setAttribute('data-slide', i+1))
+
+      // add helper classes when the carousel loads
+      this.container.firstElementChild.classList.add('is-active')
+      this.container.firstElementChild.nextElementSibling.classList.add('is-next')
+      this.container.lastElementChild.classList.add('is-previous')
+
+      // set current item count
+      this.currentItem = this.carousel.querySelector(`[data-slide="${this.currentCount}"]`)
+    })
+  }
+
+  transitionItem() {
+    // get current active slide
+    this.activeItem = this.carousel.querySelector('.is-active')
+    this.currentItem = this.carousel.querySelector(`[data-slide="${this.currentCount}"]`)
+
+    if (this.currentCount === 1) {
+      // if we're at the beginning
+      this.previousItem = this.container.lastElementChild
+      this.nextItem = this.currentItem.nextElementSibling
+    } else if (this.currentCount === this.slideCount) {
+      // if we're at the end
+      this.previousItem = this.currentItem.previousElementSibling
+      this.nextItem = this.container.firstElementChild
+    } else {
+      // if we're somewhere in the middle
+      this.previousItem = this.currentItem.previousElementSibling
+      this.nextItem = this.currentItem.nextElementSibling
+    }
+
+    // remove class names
+    this.slides.forEach(el => el.className = '')
+
+    // update class names
+    this.currentItem.className = 'is-active'
+    this.previousItem.className = 'is-previous'
+    this.nextItem.className = 'is-next'
+  }
+}
+
+// class Carousel {
+//   constructor() {
+//     this.container = document.querySelector('.carousel--container')
+//     this.controlsPrevious = document.querySelector('.carousel--controls-prev')
+//     this.controlsNext = document.querySelector('.carousel--controls-next')
+//     this.slides = Array.from(this.container.getElementsByTagName('li'))
+//     this.slideCount = this.container.getElementsByTagName('li').length
+//
+//     this.indicators = document.querySelector('.carousel--indicators')
+//     this.dots = Array.from(this.indicators.querySelectorAll('li'))
+//     this.indicatorsLength = this.indicators.querySelectorAll('li').length
+//
+//     this.currentCount = 1
+//     this.currentItem = ''
+//     this.activeItem = ''
+//     this.previousItem = ''
+//     this.nextItem = ''
+//
+//     this.addEventListeners();
+//   }
+//
+//   addEventListeners() {
+//     // previous arrow moves to previous slide
+//     this.controlsPrevious.addEventListener('click', e => {
+//       e.preventDefault()
+//       this.currentCount === 1 ? this.currentCount = this.slideCount : this.currentCount--
+//       this.transitionItem()
+//     })
+//
+//     // next arrow moves to next slide
+//     this.controlsNext.addEventListener('click', e => {
+//       e.preventDefault()
+//       this.currentCount === this.slideCount ? this.currentCount = 1 : this.currentCount++
+//       this.transitionItem()
+//     })
+//
+//     // dot moves to appropriate slide
+//     this.dots.forEach((dot, i) => {
+//       dot.addEventListener('click', e => {
+//         e.preventDefault()
+//         console.log(currentItem);
+//       })
+//     })
+//
+//     document.addEventListener('DOMContentLoaded', () => {
+//
+//       // add data attributes to slides
+//       this.slides.forEach((el, i) => el.setAttribute('data-slide', i+1))
+//
+//       // add data attributes to indicators
+//       this.dots.forEach((el, i) => el.setAttribute('data-slide', i+1))
+//
+//       // add helper classes when the carousel loads
+//       this.container.firstElementChild.classList.add('carousel--item-active')
+//       this.container.firstElementChild.nextElementSibling.classList.add('carousel--item-next')
+//       this.container.lastElementChild.classList.add('carousel--item-previous')
+//
+//       // set current item count
+//       this.currentItem = document.querySelector(`[data-slide="${this.currentCount}"]`)
+//     })
+//   }
+//
+//   updateDots() {
+//     this.dots.forEach((dot, i) => {
+//       dot.classList.remove('is-active')
+//       if (i == this.currentSlide) dot.classList.add('is-active')
+//     })
+//   }
+//
+//   transitionItem() {
+//     // get current active slide
+//     this.activeItem = document.querySelector('.carousel--item-active')
+//     this.currentItem = document.querySelector(`[data-slide="${this.currentCount}"]`)
+//
+//     if (this.currentCount == 1) {
+//       // if we're at the beginning
+//       this.previousItem = this.container.lastElementChild
+//       this.nextItem = this.currentItem.nextElementSibling
+//     } else if (this.currentCount == this.slideCount) {
+//       // if we're at the end
+//       this.previousItem = this.currentItem.previousElementSibling
+//       this.nextItem = this.container.firstElementChild
+//     } else {
+//       // if we're somewhere in the middle
+//       this.previousItem = this.currentItem.previousElementSibling
+//       this.nextItem = this.currentItem.nextElementSibling
+//     }
+//
+//     // remove class names
+//     this.slides.forEach(el => el.className = '')
+//
+//     // update class names
+//     this.currentItem.className = 'carousel--item-active'
+//     this.previousItem.className = 'carousel--item-previous'
+//     this.nextItem.className = 'carousel--item-next'
+//   }
+// }
+//
+// export default Carousel;
+
+export default Carousel;
--- _src/assets/javascripts/main.js
+++ _src/assets/javascripts/main.js
@@ -1,7 +1,15 @@
 import Navigation from './components/Navigation'
 import Theme from './components/Theme'
 import Search from './components/Search'
+import Carousel from './components/Carousel'
 
 if (document.querySelector('.navigation__header')) new Navigation();
 if (document.querySelector('select[name="themes"]')) new Theme();
 if (document.querySelector('.search')) new Search();
+
+if (document.querySelector('.carousel')) {
+	const carousels = document.querySelectorAll('.carousel');
+	for (let carouselsIndex = 0, length = carousels.length; carouselsIndex < length; carouselsIndex++) {
+		new Carousel(carousels[carouselsIndex]);
+	}
+}
